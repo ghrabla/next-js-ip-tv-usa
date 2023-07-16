985ae08a738027c56b4480a4e28d1552997d30eb
Kamal Rhrabla <Kamalghrabla@gmail.com>
2023-07-16 19:45:17 +0100

    wip

--- src/app/page.js
+++ src/app/page.js
@@ -1,12 +1,14 @@
 import Footer from "@/components/footer"
 import Container from "@/components/container"
 import Navbar from "@/components/navbar"
+import Cockies from "@/components/cockies"
 
 export default function Home() {
   return (
      <div>
       <Navbar></Navbar>
      <Container></Container>
+     <Cockies></Cockies>
      <Footer></Footer>
      </div>
   )
