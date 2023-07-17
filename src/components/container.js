b316043f4e2f39bc9644e6ebad1ad3eeafc72676
Kamal Rhrabla <Kamalghrabla@gmail.com>
2023-07-17 09:08:35 +0100

    wip

--- src/components/container.js
+++ src/components/container.js
@@ -1,35 +1,38 @@
+import Cards from "./cards";
+
 const Container = () => {
     return (
 
         <div>
-            <section class="px-2 py-32 bg-white md:px-0">
-                <div class="container items-center max-w-6xl px-8 mx-auto xl:px-5">
-                    <div class="flex flex-wrap items-center sm:-mx-3">
+            <section  className="text-white bg-cover bg-no-repeat bg-fixed opacity-80"
+        style={{
+          backgroundImage: `url(https://assets.bwbx.io/images/users/iqjWHBFdfxIU/iuxzQJrRBiN8/v0/-1x-1.jpg)`,
+          height: '90vh'
+          
+        }}>
+                <div class=" max-w-6xl px-8 mx-auto xl:px-5 pt-16">
+                    <div class="flex flex-wrap sm:-mx-3">
                         <div class="w-full md:w-1/2 md:px-3">
                             <div class="w-full pb-6 space-y-6 sm:max-w-md lg:max-w-lg md:space-y-4 lg:space-y-8 xl:space-y-9 sm:pr-5 lg:pr-0 md:pb-0">
                                 <h1 class="text-4xl font-extrabold tracking-tight text-gray-900 sm:text-5xl md:text-4xl lg:text-5xl xl:text-6xl">
-                                    <span class="block xl:inline">Useful Tools to </span>
+                                    <span class="block text-white xl:inline">Useful Tools to </span>
                                     <span class="block text-orange-500 xl:inline">Help You Build Faster.</span>
                                 </h1>
-                                <p class="mx-auto text-base text-gray-500 sm:max-w-md lg:text-xl md:max-w-3xl">It's never been easier to build beautiful websites that convey your message and tell your story.</p>
+                                <p class="mx-auto text-base text-gray-100 sm:max-w-md lg:text-xl md:max-w-3xl">It's never been easier to build beautiful websites that convey your message and tell your story.</p>
                                 <div class="relative flex flex-col sm:flex-row sm:space-x-4">
                                     <a href="#_" class="flex items-center w-full px-6 py-3 mb-3 text-lg text-white bg-orange-500 rounded-md sm:mb-0 hover:bg-orange-500 sm:w-auto">
                                         Try It Free
                                         <svg xmlns="http://www.w3.org/2000/svg" class="w-5 h-5 ml-1" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"><line x1="5" y1="12" x2="19" y2="12"></line><polyline points="12 5 19 12 12 19"></polyline></svg>
                                     </a>
-                                    <a href="#_" class="flex items-center px-6 py-3 text-gray-500 bg-gray-100 rounded-md hover:bg-gray-200 hover:text-gray-600">
-                                        Learn More
-                                    </a>
                                 </div>
-                            </div>
-                        </div>
-                        <div class="w-full md:w-1/2">
-                            <div class="w-full h-auto overflow-hidden rounded-md shadow-xl sm:rounded-xl">
-                                <img src="https://images.unsplash.com/photo-1498049860654-af1a5c566876?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1050&q=80" />
                             </div>
                         </div>
                     </div>
                 </div>
+            </section>
+
+            <section className="w-full bg-white pt-7 pb-7 md:pt-20 md:pb-24">
+              <Cards></Cards>
             </section>
 
             <section class="w-full bg-white pt-7 pb-7 md:pt-20 md:pb-24">
